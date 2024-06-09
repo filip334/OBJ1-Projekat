97c3b07f31880eda120dddac175cb740df17d3c9
filip334 <todorovicf239@gmail.com>
2024-06-09 23:40:30 +0200

    Update

--- OBJ1 Projekat/main.cpp
+++ OBJ1 Projekat/main.cpp
@@ -19,8 +19,8 @@ int Vozilo::brojProizvedenihVozila = 0;
 	out << vozilo.informacijeVozila();
 	return out;
 }*/
-template <typename T> 
-void upisUDatoteku(vector<T> vektor,const string& imeFajla) {
+template <typename T>
+void upisUDatoteku(vector<T> vektor, const string& imeFajla) {
 	ofstream outFile(imeFajla, ios::app);
 	if (!outFile) {
 		cerr << "Greska u otvaranju datoteke " << imeFajla << endl;
@@ -43,7 +43,7 @@ void upisObjektaUDatoteku(const T& objekat, const string& imeFajla) {
 		std::cerr << "Error writing to file: " << imeFajla << endl;
 	}
 	else {
-		std::cout << "Successfully wrote to file: "<< imeFajla << endl;
+		std::cout << "Successfully wrote to file: " << imeFajla << endl;
 	}
 	outFile.close();
 
@@ -116,11 +116,160 @@ void createTextFile(const std::vector<Vozilo*>& vozila, const std::string& filen
 }
 
 int main() {
-	vector<Vozilo*> fabrikaVozila;
-	stack<Vozilo*> automobili;
-	queue<Vozilo*> kamioni;
-	bool program = true;
-	/*Automobil make_unique a("Audi", "A4", 2016, "Limuzina", "Prednji", 5);
+
+	//vector<Vozilo*> fabrikaVozila;
+
+	string komanda;
+	stack<Vozilo*> stackVozila;
+	queue<Vozilo*> queueVozila;
+	Vozilo* vozilo = nullptr;
+
+	//KOMANDE
+	map<std::string, int> komande = {
+		{"unosA", 1},
+		{"unosK", 2},
+		{"ispisStek", 3},
+		{ "ispisQueue", 4 },
+		{ "upisStek", 5 },
+		{ "upisQueue", 6 },
+		{ "upisDatoteka", 7 },
+		{ "prikaz", 8 },
+		{ "brisanje", 9 },
+		{ "pomoc", 10 },
+		{ "izlaz", 11 }
+	};
+
+	while (true) {
+		cout << "Komanda (unesi pomoc za pomoc): "; cin >> komanda;
+
+		if (komande.find(komanda) == komande.end()) {
+			cout << "Nepoznata komanda\n";
+			continue;
+		}
+
+		int izbor = komande[komanda];
+
+		switch (izbor)
+		{
+		case 1: {
+			string marka, model, pogon, tip;
+			int godina, brojVrata;
+			cout << "--Unos automobila--\nMarka:"; cin >> marka;
+			cout << "Model:"; cin >> model; fflush(stdin);
+			cout << "Pogon:"; cin >> pogon; fflush(stdin);
+			cout << "Tip(Limuzina,Hecbek,Karavan,...):"; cin >> tip; fflush(stdin);
+			cout << "Godina:"; cin >> godina;
+			cout << "Broj vrata"; cin >> brojVrata;
+			Automobil* a = new Automobil(marka, model, godina, tip, pogon, brojVrata);
+			cout << "\n--Uneto vozilo--\n";
+			cout << a->informacijeVozila() << endl;
+			vozilo = a;
+			break;
+		}
+		case 2: {
+			string marka, model;
+			int godina, nosivost;
+			cout << "--Unos kamiona--\nMarka:";
+			cin >> marka; fflush(stdin);
+			cout << "Model:"; cin >> model; fflush(stdin);
+			cout << "Godina:"; cin >> godina;
+			cout << "Nosivost:"; cin >> nosivost;
+			Kamion* k = new Kamion(marka, model, godina, nosivost);
+			cout << "\n--Uneto vozilo--\n";
+			cout << k->informacijeVozila() << endl;
+			vozilo = k;
+			break;
+		}
+		case 3: //ispis Stack
+			if (stackVozila.empty()) {
+				std::cout << "Stek je prazan." << std::endl;
+			}
+			else {
+				std::stack<Vozilo*> tempStack = stackVozila;
+				while (!tempStack.empty()) {
+					tempStack.top()->informacijeVozila();
+					tempStack.pop();
+				}
+			}
+			break;
+		case 4://ispis Queue
+			if (queueVozila.empty()) {
+				std::cout << "Queue je prazan." << std::endl;
+			}
+			else {
+				std::queue<Vozilo*> tempQueue = queueVozila;
+				while (!tempQueue.empty()) {
+					tempQueue.front()->informacijeVozila();
+					tempQueue.pop();
+				}
+			}
+			break;
+		case 5: //upis Stack
+			if (vozilo) {
+				stackVozila.push(vozilo);
+				vozilo = nullptr;
+				std::cout << "Vozilo je upisano u stek." << std::endl;
+			}
+			else {
+				std::cout << "Nema trenutnog vozila za upis u stek." << std::endl;
+			}
+			break;
+		case 6: //upis Queue
+			if (vozilo) {
+				queueVozila.push(vozilo);
+				vozilo = nullptr;
+				std::cout << "Vozilo je upisano u queue." << std::endl;
+			}
+			else {
+				std::cout << "Nema trenutnog vozila za upis u queue." << std::endl;
+			}
+			break;
+		case 7:
+			break;
+		case 8:
+			cout << "--Trenutno vozilo--\n";
+			if (vozilo) {
+				cout << vozilo->informacijeVozila() << endl;
+			}
+			else {
+				cout << "Nema trenutnog vozila" << endl;
+			}
+			break;
+		case 9:
+			delete vozilo;
+			vozilo = nullptr;
+			cout << "Trenutni objekat obrisan." << endl;
+			break;
+		case 10:
+			cout << "Komande:\n"
+				<< "unosA - Unos Automobila\n"
+				<< "unosK - Unos Kamiona\n"
+				<< "prikaz - Prikazuje trenutni objekat\n"
+				<< "pomoc - Pomoc\n"
+				<< "ispisStek - Ispis steka\n"
+				<< "ispisQueue - Ispis queue\n"
+				<< "upisStek - Upisuje trenutni objekat u stek\n"
+				<< "upisQueue - Upisuje trenutni objekat u queue\n"
+				<< "brisanje - Brise trenutni objekat\n"
+				<< "izlaz - Kraj programa\n";
+			break;
+		case 11:
+
+			delete vozilo;
+			return 0;
+
+		default:
+			cout << "Nepoznata komanda." << endl;
+		}
+	}
+
+	return 0;
+}
+
+
+
+/*Automobil make_unique a("Audi", "A4", 2016, "Limuzina", "Prednji", 5);
+>>>>>>> Stashed changes
 	Kamion k("Mercedes", "Actros", 2010, 15000);
 	fabrikaVozila.push_back(new Automobil("Audi", "A4", 2016, "Limuzina", "Prednji", 5));
 	fabrikaVozila.push_back(new Kamion("Mercedes", "Actros", 2010, 15000));
@@ -129,108 +278,5 @@ int main() {
 	//fabrikaVozila.push_back(a);
 	//cout << k.informacijeVozila() << endl;
 	//fabrikaVozila.push_back(k);*/
-	while (program) {
-		char komanda;
-		cout << "Komanda (unsei p za pomoc):  "; cin >> komanda; fflush(stdin);
-		switch (komanda)
-		{
-		case 'p': 
-			cout << "\n\nKomanda:\nu - Unos objekta\np - Pomoc\ni - Ispis svih objekata\ns - Ispis iz steka\nq - Ispis iz queue\nk - Kraj programa\n\n"; 
-			break;
 
-		case 'u':
-			char objekat;
-			cout << "--Unos objekata--"; 
-			do{
-				cout << "Izaberi objekat za unos\na - Za automobil\nk - Za kamion\np - Za prekid unosa"; cin >> objekat; fflush(stdin);
-				if (objekat != 'p' || objekat != 'a' || objekat != 'k') {
-					cout << "--NEISPRAVAN UNOS!!--";
-				}
-			} while (objekat != 'p' && objekat != 'a' && objekat != 'k');
-			
-			if (objekat == 'a') {
-				string marka, model, pogon, tip;
-				int godina, brojVrata;
-				cout << "--Unos automobila--\nMarka:";
-				cin >> marka; 
-				cout << "Model:"; cin >> model; fflush(stdin);
-				cout << "Pogon:"; cin >> pogon; fflush(stdin);
-				cout << "Tip(Limuzina,Hecbek,Karavan,...):"; cin >> tip; fflush(stdin);
-				cout << "Godina:"; cin >> godina;
-				cout << "Broj vrata"; cin >> brojVrata;
-				Automobil* a = new Automobil(marka, model, godina, tip, pogon, brojVrata);
-			}
-			else if (objekat == 'k') {
-				string marka, model;
-				int godina, nosivost;
-				cout << "--Unos kamiona--\nMarka:";
-				cin >> marka; fflush(stdin);
-				cout << "Model:"; cin >> model; fflush(stdin);
-				cout << "Godina:"; cin >> godina; 
-				cout << "Nosivost:"; cin >> nosivost;
-				Kamion* k = new Kamion(marka, model,godina,nosivost);
-			}
-			else {
-				cout << "OTKAZUJEM...";
-			}
-			break;
 
-		case 'k': 
-
-			program = false; 
-			return 0;
-
-		default:
-			break;
-		}
-	}
-	/*cout << "\n\nBroj proizvedenih vozila je: " << Vozilo::GetBrojProizvedenihVozila() << "\n\n";
-
-		ucitajVozila("voziloCitajVozila.txt", fabrikaVozila);
-		cout << "===========================================================\n";
-		for (const auto& vozila : fabrikaVozila) {
-			cout << *vozila << endl;
-			if (dynamic_cast<Automobil*>(vozila)) {
-				automobili.push(vozila);
-				cout << "1";
-			}
-			else if (dynamic_cast<Kamion*>(vozila)) {
-				kamioni.push(vozila);
-				cout << "2";
-			}
-
-		}
-		cout << "===========================================================\n";
-		cout << "\nBroj proizvedenih vozila je: " << Vozilo::GetBrojProizvedenihVozila() << "\n\n";
-
-		if (!fabrikaVozila.empty()) {
-			qsort(&fabrikaVozila[0], fabrikaVozila.size(), sizeof(Vozilo*), porediVozila);
-		}
-		cout << "====================Sortirano po modelu====================\n";
-		for (const auto& vozila : fabrikaVozila) {
-			cout << *vozila << endl;
-		}
-		cout << "===========================================================\n";
-		createTextFile(fabrikaVozila, "json.txt");
-		cout << "Stack contents:" << endl;
-
-		while (!automobili.empty()) {
-			Vozilo* v = automobili.top();
-			cout << *v << endl;
-			automobili.pop();
-		}
-
-		// Demonstrating queue usage
-		cout << "Queue contents:" << endl;
-		while (!kamioni.empty()) {
-			Vozilo* v = kamioni.front();
-			cout << *v << endl;
-			kamioni.pop();
-		}
-
-	for (auto v : fabrikaVozila) {
-		delete v;
-	}
-	return 0;*/
-	return 0;
-}
